159dfe5755e6d614277e329577fcaad22c0c4433
artemN63 <nikishynartem@gmail.com>
2025-09-19 18:53:34 -0500

    fixed hard-coding

--- tests/home-page-test.spec.ts
+++ tests/home-page-test.spec.ts
@@ -1,18 +1,17 @@
 import {test, expect} from '@playwright/test'
 import { HomePage } from '../pages/home-page'
 
+let homePage: HomePage;
+
 test.beforeEach(async ({page}) => {
+    homePage = new HomePage(page)
     await page.goto('https://www.cars.com/')
 })
 
 test('Main title validation', async ({page}) => {
-    let homePage = new HomePage(page)
-
     await homePage.mainTitleValidation()
 })
 
 test('Filter title validation', async ({page}) => {
-    let homePage = new HomePage(page)
-
     await homePage.filterTitleValidation()
 })
